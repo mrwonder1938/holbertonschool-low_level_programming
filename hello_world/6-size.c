2985932a2bb2d0d5955b73a7efc402e7db6e3bb9
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-17 12:22:22 +0000

    6. Size is not grandeur, and territory does not make a nation mandatory Write a C program that prints the size of various types on the computer it is compiled and run on.
    You should produce the exact same output as in the example
    Warnings are allowed
    Your program should return 0
    If you are using a linux on Vagrant you might have to install the package libc6-dev-i386 to test the -m32 gcc option (normally you dont need to do anything on your sandbox).

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("The sizeof(char) is %zu bytes.\n", sizeof(char));
+    printf("The sizeof(short) is %zu bytes.\n", sizeof(short));
+    printf("The sizeof(int) is %zu bytes.\n", sizeof(int));
+    printf("The sizeof(long) is %zu bytes.\n", sizeof(long));
+    printf("The sizeof(long long) is %zu bytes.\n", sizeof(long long));
+    printf("The sizeof(float) is %zu bytes.\n", sizeof(float));
+    printf("The sizeof(double) is %zu bytes.\n", sizeof(double));
+    printf("The sizeof(long double) is %zu bytes.\n", sizeof(long double));
+
+    return 0;
+}
