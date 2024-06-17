84f6e7832c83c37d8d9ca843a2bc75ad07d78d38
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-17 12:24:30 +0000

    6. Size is not grandeur, and territory does not make a nation mandatory Write a C program that prints the size of various types on the computer it is compiled and run on.
    You should produce the exact same output as in the example
    Warnings are allowed
    Your program should return 0
    If you are using a linux on Vagrant you might have to install the package libc6-dev-i386 to test the -m32 gcc option (normally you dont need to do anything on your sandbox).

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("The sizeof(char) is %zu bytes.\n", sizeof(char));
-    printf("The sizeof(short) is %zu bytes.\n", sizeof(short));
-    printf("The sizeof(int) is %zu bytes.\n", sizeof(int));
-    printf("The sizeof(long) is %zu bytes.\n", sizeof(long));
-    printf("The sizeof(long long) is %zu bytes.\n", sizeof(long long));
-    printf("The sizeof(float) is %zu bytes.\n", sizeof(float));
-    printf("The sizeof(double) is %zu bytes.\n", sizeof(double));
-    printf("The sizeof(long double) is %zu bytes.\n", sizeof(long double));
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-    return 0;
-}
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+
