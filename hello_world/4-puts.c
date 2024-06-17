43ae60355e6f250001b2441e2e935c1eb71b3e21
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-17 10:11:58 +0000

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;}
+int main() {
+    puts("Programming is like building a multilingual puzzle,");
+    puts("");
+    return 0;
+}
