159a99450e047ed91fe7c6f3d681153a304f902c
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-18 07:38:52 +0000

    0. Positive anything is better than negative nothing mandatory This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
    You can find the source code here
    The variable n will store a different value every time you will run this program
    You don’t have to understand what rand, srand, RAND_MAX do. Please do not touch this code
    The output of the program should be:
    The number, followed by
    if the number is greater than 0: is positive
    if the number is 0: is zero
    if the number is less than 0: is negative
    followed by a new line

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 int main(void)
@@ -20,5 +22,5 @@ int main(void)
         printf("%d is negative\n", n);
     }
 
-    return (0);
+    return 0;
 }
