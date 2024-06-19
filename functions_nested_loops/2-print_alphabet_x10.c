dc834eb0c44dc34f37586141b3d0a87843e76ad5
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-19 12:32:41 +0000

    2. 10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * print_alphabet_x10 - Make alphabet x10 times
-  *
-  * Return: void
-  */
+ * print_alphabet_x10 - prints alphabet letters ten times
+ * 
+ * Description: The function uses a loop to print the alphabet
+ *              followed by a newline, ten times.
+ * 
+ * Return: void
+ */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+    int counter = 0;
+    int letter;
 
-	while (i <= 9)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-		}
+    while (counter++ < 10) // Adjusted to run exactly ten times
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+            _putchar(letter);
 
-		_putchar('\n');
-
-		i++;
-	}
+        _putchar('\n'); // Changed 10 to '\n' for clarity
+    }
 }
