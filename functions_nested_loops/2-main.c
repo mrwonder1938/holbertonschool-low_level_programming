009d8ba0a92e386634799458a15b56d361d69d18
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-21 12:05:53 +0000

    10 x alphabet

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -12,14 +12,3 @@ int main(void)
 }   
 julien@ubuntu:~/$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 2-main.c 2-print_alphabet_x10.c -o 2-alphabet_x10
 julien@ubuntu:~/$ ./2-alphabet_x10
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-abcdefghijklmnopqrstuvwxyz
-julien@ubuntu:~/$
