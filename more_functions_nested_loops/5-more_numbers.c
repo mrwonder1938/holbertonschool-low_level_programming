313d9f8d75fed68d77324fd8303e48253131c733
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 14:02:22 +0000

    Numbers constitute the only universal language

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,27 +6,27 @@
  */
 void more_numbers(void)
 {
-    int i, j, k;
-    char buffer[21]; /* Buffer to hold numbers from 0-14 and newline character */
+	int i, j, k;
+	char buffer[21]; /* Buffer to hold numbers from 0-14 and newline character */
 
-    for (i = 0; i < 10; i++)
-    {
-        char *ptr = buffer;
+	for (i = 0; i < 10; i++)
+	{
+		char *ptr = buffer;
 
-        for (j = 0; j <= 14; j++)
-        {
-            if (j > 9)
-            {
-                *ptr++ = (j / 10) + '0'; /* Tens place */
-            }
-            *ptr++ = (j % 10) + '0'; /* Units place */
-        }
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				*ptr++ = (j / 10) + '0'; /* Tens place */
+			}
+			*ptr++ = (j % 10) + '0'; /* Units place */
+		}
 
-        *ptr++ = '\n'; /* Newline character */
+		*ptr++ = '\n'; /* Newline character */
 
-        for (k = 0; k < (ptr - buffer); k++)
-        {
-            _putchar(buffer[k]);
-        }
-    }
+		for (k = 0; k < (ptr - buffer); k++)
+		{
+			_putchar(buffer[k]);
+		}
+	}
 }
