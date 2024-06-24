c11a5b84426858cd815461d92c478c72f40147bc
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 13:25:22 +0000

    The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_numbers(void)
 {
-    int num;
+	int num;
 
-    num = 0;
-    while (num <= 9)
-    {
-        _putchar(num + '0'); /* Print the digit character */
-        num++;
-    }
-    _putchar('\n'); /* Print newline character after all numbers */
+	num = 0;
+	while (num <= 9)
+	{
+		_putchar(num + '0'); /* Print the digit character */
+		num++;
+	}
+	_putchar('\n'); /* Print newline character after all numbers */
 }
