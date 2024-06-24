a7d5973d7cd319bbf0f9e70ffe1226cce3b20c2a
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 12:44:25 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,13 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return 1; /* c is uppercase */
-    }
-    else
-    {
-        return 0; /* c is not uppercase */
-    }
+	/* Check if c is within the uppercase ASCII range */
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1); /* c is uppercase */
+	}
+	else
+	{
+		return (0); /* c is not uppercase */
+	}
 }
