026845b1819a26ba3c6560ac0507427486ae7b64
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 13:17:10 +0000

    The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers- function that prints the numbers
- *
+ * print_numbers - prints numbers from 0 to 9 followed by a new line
  */
-
 void print_numbers(void)
 {
-	char n = '0';
+    int num;
 
-	while (n <= '9')
-	{
-		_putchar(n++);
-	}
-	_putchar('\n');
+    num = 0;
+    while (num <= 9)
+    {
+        _putchar(num + '0'); // Print the digit character
+        num++;
+    }
+    _putchar('\n'); // Print newline character after all numbers
 }
