ea8a7708971eb17e7fbe64e0e0879370a21706e6
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 12:57:02 +0000

    Collaboration is multiplication

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for _isdigit */
-int _isdigit(int c);
+/* Function prototype for mul */
+int mul(int a, int b);
 
 /* Other function prototypes if any */
 
