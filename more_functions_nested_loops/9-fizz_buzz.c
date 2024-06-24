9f44e88aff6b510f4ec7c4f2d48dbc7f68975ee4
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-24 14:36:51 +0000

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,36 +1,64 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void)
-{
-    int i;
+// Declaration of _putchar function
+int _putchar(char c) {
+    return write(1, &c, 1);  // Using write system call to output a single character
+}
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
+// Function to print FizzBuzz sequence
+void fizzbuzz(void) {
+    for (int i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            // Print "FizzBuzz"
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
+        } else if (i % 3 == 0) {
+            // Print "Fizz"
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+        } else if (i % 5 == 0) {
+            // Print "Buzz"
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
+        } else {
+            // Print the number i
+            int temp = i;
+            int digits = 0;
+            while (temp != 0) {
+                temp /= 10;
+                digits++;
+            }
+            temp = i;
+            char buffer[10];  // Buffer to hold digits of i
+            int idx = digits - 1;
+            while (temp != 0) {
+                buffer[idx--] = temp % 10 + '0';
+                temp /= 10;
+            }
+            for (int j = 0; j < digits; j++) {
+                _putchar(buffer[j]);
+            }
         }
 
         // Print space after each number or word, except after the last one
-        if (i != 100)
-        {
-            printf(" ");
+        if (i != 100) {
+            _putchar(' ');
         }
     }
+    _putchar('\n');
+}
 
-    printf("\n");
-
-    return 0;
+int main(void) {
+    fizzbuzz();  // Call the fizzbuzz function
+    return (0);
 }
