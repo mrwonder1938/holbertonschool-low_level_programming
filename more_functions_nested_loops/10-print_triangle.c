93750c89b7c866303d1b7a1f195c85135f18d6fb
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 07:58:28 +0000

    10. Triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * print_triangle - Prints a triangle followed by a new line
+ * @size: The size of the triangle
+ *
+ * Description: This function prints a right-aligned triangle
+ *              using the '#' character. If the size is 0 or less,
+ *              it prints only a new line.
+ */
 void print_triangle(int size)
 {
 	int i, j, k;
