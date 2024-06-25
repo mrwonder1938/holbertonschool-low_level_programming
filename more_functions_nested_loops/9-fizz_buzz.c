2b8064489239e14d6081ee4461f2e0f90632b6ab
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 08:02:16 +0000

    9. Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,66 +1,52 @@
-#include <unistd.h>
+#include <stdio.h>
 
-/* Declaration of _putchar function */
-int _putchar(char c) {
-    return write(1, &c, 1);  /* Using write system call to output a single character */
-}
-
-/* Function to print FizzBuzz sequence */
-void fizzbuzz(void) {
+/**
+ * fizzbuzz - Prints the numbers from 1 to 100, but for multiples of three
+ * prints "Fizz" instead of the number and for the multiples of five
+ * prints "Buzz". For numbers which are multiples of both three and five
+ * prints "FizzBuzz".
+ */
+void fizzbuzz(void)
+{
     int i;
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            /* Print "FizzBuzz" */
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        } else if (i % 3 == 0) {
-            /* Print "Fizz" */
-            _putchar('F');
-            _putchar('i');
-            _putchar('z');
-            _putchar('z');
-        } else if (i % 5 == 0) {
-            /* Print "Buzz" */
-            _putchar('B');
-            _putchar('u');
-            _putchar('z');
-            _putchar('z');
-        } else {
-            /* Print the number i */
-            int temp = i;
-            int digits = 0;
-            while (temp != 0) {
-                temp /= 10;
-                digits++;
-            }
-            temp = i;
-            char buffer[10];  /* Buffer to hold digits of i */
-            int idx = digits - 1;
-            while (temp != 0) {
-                buffer[idx--] = temp % 10 + '0';
-                temp /= 10;
-            }
-            int j;  // Move declaration to the beginning of the block
-            for (j = 0; j < digits; j++) {
-                _putchar(buffer[j]);
-            }
+
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            /* Buffer to hold digits of i */
+            char buffer[10];
+            sprintf(buffer, "%d", i);
+            printf("%s", buffer);
         }
 
-        /* Print space after each number or word, except after the last one */
-        if (i != 100) {
-            _putchar(' ');
+        if (i != 100)
+        {
+            printf(" ");
         }
     }
-    _putchar('\n');
+    printf("\n");
 }
 
-int main(void) {
-    fizzbuzz();  /* Call the fizzbuzz function */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    fizzbuzz();
     return (0);
 }
