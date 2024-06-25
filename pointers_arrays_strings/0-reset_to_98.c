d17a635244600d02db7506aaef8f42383b457a6d
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 12:44:33 +0000

    0. 98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,22 @@
-// Function implementation
+#include <stdio.h>
+
+// Function prototype
+void reset_to_98(int *n);
+
+int main() {
+    int num = 5;
+
+    printf("Before reset_to_98: %d\n", num);
+
+    reset_to_98(&num);  // Pass the address of num to the function
+
+    printf("After reset_to_98: %d\n", num);
+
+    return 0;
+}
+
+
+/* Function implementation */
 void reset_to_98(int *n) {
     *n = 98;  /* Dereference the pointer and update the value it points to 98 */
 }
