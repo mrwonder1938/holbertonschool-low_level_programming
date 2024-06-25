575fbb44947e8a0398d86b659c613185914188ee
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 13:02:47 +0000

    0. 98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
 /* Function implementation */
 void reset_to_98(int *n)
 {
-    *n = 98;  /* Dereference the pointer and update the value it points to 98 */
+    *n = 98; /* Dereference the pointer and update the value it points to 98 */
 }
