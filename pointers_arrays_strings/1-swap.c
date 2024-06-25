0af0804812630602141ac276d27830908e289722
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 13:33:11 +0000

    1. Don't swap horses in crossing a stream

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * swap_int - Swaps the values of two integers.
@@ -7,25 +7,9 @@
  */
 void swap_int(int *a, int *b)
 {
-    int temp; // Temporary variable to hold the value during swap
+    int temp; /* Temporary variable to hold the value during swap */
 
     temp = *a; // Store the value at address a in temp
     *a = *b;   // Assign the value at address b to address a
     *b = temp; // Assign the value in temp to address b
-}
-
-// Example usage of the swap_int function
-int main(void)
-{
-    int x = 10;
-    int y = 20;
-
-    printf("Before swap: x = %d, y = %d\n", x, y);
-
-    // Call the swap_int function to swap values of x and y
-    swap_int(&x, &y);
-
-    printf("After swap: x = %d, y = %d\n", x, y);
-
-    return 0;
 }
