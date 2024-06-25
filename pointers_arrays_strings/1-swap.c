30ccbdb110cfe846fc894c720b4510613064e65c
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-25 14:19:01 +0000

    1. Don't swap horses in crossing a stream

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -5,9 +5,7 @@
  * @a: Pointer to the first integer.
  * @b: Pointer to the second integer.
  */
-
 void swap_int(int *a, int *b)
-
 {
     int temp; /* Temporary variable to hold the value during swap */
 
@@ -15,4 +13,3 @@ void swap_int(int *a, int *b)
     *a = *b;   /* Assign the value at address b to address a */
     *b = temp; /* Assign the value in temp to address b */
 }
-
