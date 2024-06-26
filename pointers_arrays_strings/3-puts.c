6db312240c1cf3e2e78bd35119f2132defe25185
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-26 12:38:32 +0000

    3. I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,7 +3,7 @@
 void _puts(char *str) {
     /* Iterate through each character in the string until we reach '\0' */
     while (*str != '\0') {
-        putchar(*str);  // Print the current character
+        putchar(*str);  putchar(*str);  /* Print the current character */
         str++;         // Move to the next character in the string
     }
     putchar('\n');     // Print a newline character after printing the string
