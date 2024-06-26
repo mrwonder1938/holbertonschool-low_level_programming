3e1ec9ab4a7ba13973bd697563091fa188aaa8fe
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-26 14:29:44 +0000

    6. Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,13 @@
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]); /* Print every other character */
-        i++;
-    }
-    _putchar('\n'); /* Print newline at the end */
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]); /* Print every other character */
+		i++;
+	}
+	_putchar('\n'); /* Print newline at the end */
 }
