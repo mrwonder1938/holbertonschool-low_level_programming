8a6c17422af8b43565cd313dafac5a8a4166fc84
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-26 13:12:53 +0000

    3. I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-/* Function to print a string followed by a newline to stdout */
-void _puts(char *str) {
-    while (*str != '\0') {
-        _putchar(*str);  /* Use _putchar instead of putchar */
-        str++;
-    }
-    _putchar('\n');  /* Print a newline character at the end */
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: the string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);  /* Use _putchar instead of putchar */
+		str++;
+	}
+	_putchar('\n');  /* Print a newline character at the end */
 }
-
