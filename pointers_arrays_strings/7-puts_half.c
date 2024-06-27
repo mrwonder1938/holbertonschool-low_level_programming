f830cb4b7c43050c91852f362adaf1aa1fbe3956
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-27 11:20:03 +0000

    7. Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,20 @@
 #include "main.h"
-#include <string.h> // Include for strlen function
 
 /**
- * puts_half - prints the second half of a string
- * @str: the string to be printed
- */
-void puts_half(char *str)
+* print_chessboard - print chessboard given set 2D array
+* @a: 2D array
+*/
+
+void print_chessboard(char (*a)[8])
 {
-    int length = strlen(str); // Calculate the length of the string
-    int start; // Variable to store the starting index for printing
 
-    if (length % 2 == 0)
-    {
-        start = length / 2; // If even length, start from length/2
-    }
-    else
-    {
-        start = (length - 1) / 2; // If odd length, start from (length-1)/2
-    }
+int row;
+int column;
 
-    for (int i = start; i < length; i++)
-    {
-        _putchar(str[i]); // Print each character from start to end
-    }
-    _putchar('\n'); // Print a new line at the end
+for (row = 0; row < 8; row++)
+{
+for (column = 0; column < 8; column++)
+_putchar(a[row][column]);
+_putchar('\n');
+}
 }
