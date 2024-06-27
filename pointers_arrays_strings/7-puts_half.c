9b3d846fa788f5f6dfbc781f965ebc985a17ac64
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-27 11:16:36 +0000

    7. Winning is only half of it. Having fun is the other half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,21 +7,21 @@
  */
 void puts_half(char *str)
 {
-    int length = strlen(str); // Use strlen to get the length of the string
-    int i, start;
+    int length = strlen(str); // Calculate the length of the string
+    int start; // Variable to store the starting index for printing
 
     if (length % 2 == 0)
     {
-        start = length / 2;
+        start = length / 2; // If even length, start from length/2
     }
     else
     {
-        start = (length + 1) / 2;
+        start = (length - 1) / 2; // If odd length, start from (length-1)/2
     }
 
-    for (i = start; str[i] != '\0'; i++)
+    for (int i = start; i < length; i++)
     {
-        _putchar(str[i]);
+        _putchar(str[i]); // Print each character from start to end
     }
-    _putchar('\n');
+    _putchar('\n'); // Print a new line at the end
 }
