f00de67bc218af55663d308ec1e8b996d505904a
mrwonder1938 <dussiel.kevin@yahoo.fr>
2024-06-28 08:14:12 +0000

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,47 +1,28 @@
-#include <limits.h>
 #include "main.h"
 
-int _atoi(char *s) {
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-    int found_number = 0;
+/**
+ * _atoi- converts a string to an integer
+ * @s : the string to be converted
+ * Return: the integer value of the converted string
+ *
+ */
 
-    // Skip leading non-digit characters and handle signs
-    while (s[i] != '\0') {
-        if (s[i] == '-') {
-            sign = -sign;
-        } else if (s[i] == '+') {
-            // Continue without changing the sign
-        } else if (s[i] >= '0' && s[i] <= '9') {
-            // Found the start of the number
-            found_number = 1;
-            break;
-        }
-        i++;
-    }
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
 
-    // If no number was found, return 0
-    if (!found_number) {
-        return 0;
-    }
+	do {
+		if (*s == '-')
+			sign *= -1;
 
-    // Convert digits to integer
-    while (s[i] >= '0' && s[i] <= '9') {
-        int digit = s[i] - '0';
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-        // Check for overflow
-        if (result > (INT_MAX - digit) / 10) {
-            if (sign == 1) {
-                return INT_MAX;
-            } else {
-                return INT_MIN;
-            }
-        }
+		else if (num > 0)
+			break;
+	}
 
-        result = result * 10 + digit;
-        i++;
-    }
-
-    return result * sign;
+	while (*s++);
+	return (num * sign);
 }
